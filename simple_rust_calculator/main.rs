2bc321772846cfc7c3405227b5de2c7c4534845b
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2024-05-13 09:26:21 +0300

    Update main.rs

--- simple_rust_calculator/main.rs
+++ simple_rust_calculator/main.rs
@@ -1,18 +1,21 @@
 
 use std::io;
 
+/** Takes input from user */
 fn user_input() -> String {
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin().read_line(&mut input).expect("Failed to read line!");
     return input;
 }
 
+/** Parses user input */
 fn parse_num(_user_input: &String) -> f64 {
-    return _user_input.trim().parse().expect("Invalid number");
+    return _user_input.trim().parse().expect("Invalid number!");
 }
 
+/** Parses user input */
 fn parse_op(_user_input: &String) -> char {
-    return _user_input.trim().parse().expect("Invalid operator");
+    return _user_input.trim().parse().expect("Invalid operator!");
 }
 
 fn main()
@@ -20,51 +23,63 @@ fn main()
     println!("Enter first number: ");
     let n1: f64 = parse_num(&user_input());
     
-    println!("Enter second number:");
+    println!("Enter second number: ");
     let n2: f64 = parse_num(&user_input());
 
-    println!("Enter operation (+ - * / %):");
+    println!("Enter operation [+] [-] [*] [/] [%]: ");
     let op: char = parse_op(&user_input());
     
     let mut res = 0.0;
     let mut errflag: bool = false;
 
-    if op == '+' {
+    if op == '+' 
+    {
         res = n1 + n2;
     }
-    else if op == '-' {
+    else if op == '-' 
+    {
         res = n1 - n2;
     }
-    else if op == '*' {
+    else if op == '*'
+    {
         res = n1 * n2;
     }
-    else if op == '/'{
-        if n2 == 0.0 {
+    else if op == '/'
+    {
+        if n2 == 0.0
+        {
             println!("Error: division by zero!");
             errflag = true;
         }
-        else {
+        else
+        {
             res = n1 / n2;
         }
     }
-    else if op == '%'{
-        if n2 == 0.0 {
+    else if op == '%'
+    {
+        if n2 == 0.0
+        {
             println!("Error: modulo by zero!");
             errflag = true;
         }
-        else {
+        else
+        {
             res = n1 % n2;
         }
     }
-    else {
+    else
+    {
         println!("Invalid operator '{op}'");
         errflag = true;
     }
 
-    if !errflag {
+    if !errflag
+    {
         println!("\n {n1} {op} {n2} = {res}");
     }
-    else {
+    else
+    {
         std::process::exit(1);
     }
 
